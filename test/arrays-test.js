ca7301e6074ea47d5240600e76d895463b7277a1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-11 22:34:34 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -29,7 +29,7 @@ describe('arrays', () => {
       expect(array).to.eql([1])
     })
   })
-/*
+  
   describe('destructivelyAddElementToBeginningOfArray(array, element)', () => {
     it('adds an `element` to the beginning of an `array`', () => {
       expect(destructivelyAddElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
@@ -43,7 +43,7 @@ describe('arrays', () => {
       expect(array).to.eql(['foo', 1])
     })
   })
-
+/*
   describe('addElementToEndOfArray(array, element)', () => {
     it('adds an `element` to the end of an `array`', () => {
       expect(addElementToEndOfArray([1], 'foo')).to.eql([1, 'foo'])
