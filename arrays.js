e840cb486be06d78a93f27dd4521b6c9c92b8edf
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-11 22:23:44 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+/*
+function addElementToBeginningOfArray(array,element){
+  [...element, array);
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array,element){
+  array[array.length] = element
+  return array
+}
+function destructivelyAddElementToEndOfArray(array,element){
+    var newArray = [array, ...element]
+    return newArray
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  array.slice(1)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array){
+  array.slice(0,array.length - 1)
+  return array
+}
+*/
