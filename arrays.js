1c993c6d04faa44ef7a6ed1668f165eb8614af90
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-11 22:51:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,11 +34,12 @@ function removeElementFromBeginningOfArray(array){
   array = array.slice(1)
   return array
 }
-/*
+
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
+/*
 function removeElementFromEndOfArray(array){
   array.slice(0,array.length - 1)
   return array
