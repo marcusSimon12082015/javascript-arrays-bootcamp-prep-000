21bb19f3b86817b704f324f99d3f72cf607d6337
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-11 22:41:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,11 +24,12 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array, index){
   return array[index]
 }
-/*
+
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
   return array
 }
+/*
 function removeElementFromBeginningOfArray(array){
   array.slice(1)
   return array
